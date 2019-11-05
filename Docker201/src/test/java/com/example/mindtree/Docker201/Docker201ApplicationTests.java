09b61045c13805615960839daff66aa576acd254
subhahar <ssubhahar@gmail.com>
2019-11-05 14:50:17 +0530

    Initial commit of Spring boot docker application.
    Example of simple user details is retrived & from mysql

---
+++ Docker201/src/test/java/com/example/mindtree/Docker201/Docker201ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mindtree.Docker201;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Docker201ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
